b874468cce152e145619bbd638198cf5c58ebe5a
Eddiiee <Eddiiee2001@gmail.com>
2018-11-06 19:21:25 +0800

    implemented EventDetail,Login,Logout+Bootstrap

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView , EventListView , EventDetailView
 
 urlpatterns = [
-    path("", EventListView.as_view()),
-     path("events/", EventListView.as_view())
+    path("", HomePageView.as_view(), name="home"),
+    path("events/", EventListView.as_view(),
+        name="event_list"),   
+    path("events/<int:pk>/", EventDetailView.as_view(),
+        name="event_detail")       
 ]
